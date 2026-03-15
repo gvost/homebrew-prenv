b0570730473348eaac447b298b38324251a0dd5c
gvost <ohdavidno@proton.me>
2026-03-14 19:41:21 -0500

    update hash

--- Formula/prenv.rb
+++ Formula/prenv.rb
@@ -2,7 +2,7 @@ class Prenv < Formula
   desc "Lightweight Bash CLI for inspecting, formatting, and debugging your shell environment"
   homepage "https://github.com/gvost/prenv"
   url "https://github.com/gvost/prenv/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_ME"
+  sha256 "5abbb34847446a5c0a89121c4dae311854dd1d44764fa73bb3b73f7beb7010c3"
   license "MIT"
 
   def install
